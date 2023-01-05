9de2909a555b2ef63eb3ba08e231eef0ee1b5a9b
tiy <118719796+netiy@users.noreply.github.com>
2023-01-05 16:15:33 +0800

    thing
    Github being a bitch and not pasting the code correctly. It took me a little over 3 weeks to find out though

--- code1.py
+++ code1.py
@@ -1,7 +1,16 @@
-from typing import Optional
+# Create a virtual environment
+# Change python interpreter to virtual environment (<venv-name>\scripts\python.exe)
+# Activate virtual environment 
+# Install dependencies
+    # pip install fastapi[all]
+# Start up web server
+    # uvicorn main:app --reload
+
+
 from fastapi import FastAPI
 from pydantic import BaseModel
 from random import randrange
+
 
 app = FastAPI()
 
@@ -9,9 +18,10 @@ class Template(BaseModel):
     title: str
     content: str
     publish: int = True
-    rating: Optional[int] = None
 
-my_posts = [{"title": "favourite foods", "content": "pizaz", "id": 1}, {"title": "losmnd", "content": "ff", "id": 1}]
+my_posts = [{"title": "favourite foods", "content": "pizaz", "id": 1}, {"title": "losmnd", "content": "ff", "id": 2}]
+
+
 @app.get("/")
 def root():
     return {"data": "Welcome to the homepage."}
@@ -29,3 +39,21 @@ def create_post(post: Template):
         return {f"Your post titled '{post.title}' has been published."}
     else:
         return {"Draft saved."}
+
+@app.get("/posts/{id}")
+def get_individual_post(id: int):
+    for x in my_posts:
+        if x["id"] == id:
+            return {"data": x}
+    return {f"Post with id of {id} was not found"}
+
+@app.put("/posts/{id}")
+def update_post(id:int, post: Template):
+    for x in my_posts:
+        if x["id"] == id:
+            my_posts.pop(my_posts.index(x))
+            post = dict(post)
+            post["id"] = id
+            my_posts.append(post)
+            return {"data": "Post updated"}
+    return {"data": f"Post with id of {id} was not found"}
